26a2ba0e7d4656d63c68c049792d252b790a8822
joshuadignadice24@gmail.com <joshuadignadice24@gmail.com>
2020-11-07 01:58:55 +0800

    Change API Format

--- api/views.py
+++ api/views.py
@@ -1,52 +1,16 @@
 # Create your views here.
-from todo_app.models import Todo
+from django.shortcuts import render
+from rest_framework import generics
+
+# Create your views here.
+from todo_app import models
 from .serializers import TodoSerializers
 
-from django.shortcuts import render
-from django.http import JsonResponse
+class ListTodo(generics.ListCreateAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializers
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls ={
-        'List':'/task-list/',
-        'Detail View' : '/task-detail/'
-    }
-    return Response(api_urls)
-
-@api_view(['GET'])
-def taskList(request):
-    tasks = Todo.objects.all().order_by('-id')
-    serializer = TodoSerializers(tasks, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def taskDetail(request, pk):
-    tasks = Todo.objects.get(id=pk)
-    serializer = TodoSerializers(tasks, many=False)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def taskCreate(request):
-    serializer = TodoSerializers(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def taskUpdate(request, pk):
-    tasks = Todo.objects.get(id=pk)
-    serializer = TodoSerializers(instance=tasks, data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['DELETE'])
-def taskDelete(request, pk):
-    tasks = Todo.objects.get(id=pk)
-    tasks.delete()
-    return Response()
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializers
